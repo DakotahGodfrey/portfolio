995ae170fc64eaa9dd0b631930cd8bb4fceceaed
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2021-07-30 17:26:08 -0400

    form handling?

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -7,6 +7,25 @@ import { useState } from 'react';
 import { MdSend, MdCheckCircle } from 'react-icons/md';
 export default function Contact() {
   const [submitted, setSubmitted] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState(null);
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    e.preventDefault();
+    setMessage(e.target.value);
+  };
+
   const { width } = useViewport();
   const isMobile = width < mq.xs;
   const isTablet = width < mq.sm;
@@ -18,16 +37,22 @@ export default function Contact() {
       .join('&');
   };
   function handleSubmit(e) {
+    console.log(e.target);
     e.preventDefault();
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        'form-name': event.target.getAttribute('name'),
-        ...name,
+        'form-name': e.target.getAttribute('name'),
+        Name: name,
+        Email: email,
+        Message: message,
       }),
-    });
-    setSubmitted(true);
+    })
+      .then(() => {
+        setSubmitted(true);
+      })
+      .catch((error) => setError(error));
   }
   return (
     <>
@@ -62,15 +87,35 @@ export default function Contact() {
                 <h2>Send me a message</h2>
                 <div className='control-group'>
                   <label htmlFor='name'>Name</label>
-                  <input type='text' id='name' name='name' required />
+                  <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    required
+                    value={name}
+                    onChange={(e) => handleNameChange(e)}
+                  />
                 </div>
                 <div className='control-group'>
                   <label htmlFor='email'>Email</label>
-                  <input type='email' id='email' name='email' required />
+                  <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    required
+                    value={email}
+                    onChange={(e) => handleEmailChange(e)}
+                  />
                 </div>
                 <div className='control-group'>
                   <label htmlFor='message'>Message</label>
-                  <textarea name='message' id='message' required />
+                  <textarea
+                    name='message'
+                    id='message'
+                    required
+                    value={message}
+                    onChange={(e) => handleMessageChange(e)}
+                  />
                 </div>
                 <button className='contact-submit' type='submit'>
                   Send <MdSend />
@@ -83,6 +128,15 @@ export default function Contact() {
                   <MdCheckCircle />
                 </h2>
                 <p> Thanks for being awesome, I'll be in touch soon.</p>
+              </div>
+            )}
+            {error && (
+              <div className='submitted-message'>
+                <h2 role='alert'>
+                  Message failed to send!
+                  <MdCheckCircle />
+                </h2>
+                <p> Please try again.</p>
               </div>
             )}
           </section>
