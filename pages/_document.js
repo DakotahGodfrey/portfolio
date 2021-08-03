e8864fab94272d3b795ccb28f7338c29e7bc85c2
Dakotah Godfrey <dakotah.godfrey.dev@gmail.com>
2021-08-03 11:00:01 -0400

    added favicons

--- pages/_document.js
+++ pages/_document.js
@@ -35,6 +35,23 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined&display=swap'
             rel='stylesheet'
           />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/images/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/images/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/images/favicon-16x16.png'
+          />
         </Head>
         <body>
           <Main />
